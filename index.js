137eee6f206296faaa41c82adff57915faee95db
Ben Neoh [SSW] <benjaminneoh6343@gmail.com>
2025-10-03 10:28:09 +1000

    add export

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dist_1 = require("./packages/my-lib/dist");
-console.log((0, dist_1.greet)("CJS Monorepo")); // Hello, CJS Monorepo!
-console.log((0, dist_1.add)(2, 3)); // 5
